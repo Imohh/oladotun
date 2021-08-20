9e61209afde9b27a1919fda41252534390075db6
Imoh Precious <jaypee88830@gmail.com>
2021-08-20 03:09:59 +0100

    new commit

--- faq.php
+++ faq.php
@@ -16,12 +16,15 @@ foreach ($result as $row) {
     </div>
 </div>
 
-<div class="page">
+<div class="page faq-top">
     <div class="container">
         <div class="row">            
-            <div class="col-md-6">
+            <div class="col-md-8 faq">
+
+                <div class="col-md-10">
                 
-                <div class="panel-group" id="faqAccordion">                    
+                <div class="panel-group" id="faqAccordion">
+                <h2>find out more about dotun</h2>               
 
                     <?php
                     $statement = $pdo->prepare("SELECT * FROM tbl_faq");
@@ -29,11 +32,12 @@ foreach ($result as $row) {
                     $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
                     foreach ($result as $row) {
                         ?>
-                        <div class="panel panel-default">
+                        <div class="panel">
                             <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question<?php echo $row['faq_id']; ?>">
                                 <h4 class="panel-title">
-                                    Q: <?php echo $row['faq_title']; ?>
+                                    <?php echo $row['faq_title']; ?><span><i class="fa fa-plus"></i></span>
                                 </h4>
+                                
                             </div>
                             <div id="question<?php echo $row['faq_id']; ?>" class="panel-collapse collapse" style="height: 0px;">
                                 <div class="panel-body">
@@ -47,12 +51,13 @@ foreach ($result as $row) {
                         <?php
                     }
                     ?>
+                </div>
                     
                 </div>
 
             </div>
 
-            <div class="col-md-6 bottom-faq">
+            <div class="col-md-4 bottom-faq">
                 <h2>interact with us</h2>
                 <p>you can leave a message with us and well be with you.</p>
 
