89310824a26fde72f647d7868e34dad3999ac8e3
Imoh Precious <jaypee88830@gmail.com>
2021-09-06 01:08:33 +0100

    new commit

--- header.php
+++ header.php
@@ -372,6 +372,11 @@ foreach ($result as $row) {
                     <div class="col-12">
                         <!-- Header Main Menu -->
                         <div class="main-menu">
+                            <div class="center-logo">
+								<div class="center-logos">
+									<a href="index.php"><img src="assets/uploads/<?php echo $logo; ?>" alt="logo image"></a>
+								</div>
+							</div>
                             <nav>
                                 <ul>
                                     <li>
@@ -383,6 +388,54 @@ foreach ($result as $row) {
 										$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 										foreach ($result as $row) {
 									?>
+
+
+
+
+
+									<!-- <li class="has-dropdown has-megaitem">
+                                        <a href="product-category.php?id=<?//php echo $row['tcat_id']; ?>&type=top-category"><?//php echo $row['tcat_name']; ?><i class="fa fa-angle-down"></i></a>
+                                        <div class="mega-menu">
+                                            <ul class="mega-menu-inner">
+                                            	<?php
+												//	$statement1 = $pdo->prepare("SELECT * FROM tbl_mid_category WHERE tcat_id=?");
+												//	$statement1->execute(array($row['tcat_id']));
+												//	$result1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
+												//	foreach ($result1 as $row1) {
+												?>
+                                                <li class="mega-menu-item">
+                                                    <a class="mega-menu-item-title" href="product-category.php?id=<?//php echo $row1['mcat_id']; ?>&type=mid-category"><?//php echo $row1['mcat_name']; ?></a>
+                                                    <ul class="mega-menu-sub">
+                                                    	<?php
+															//$statement2 = $pdo->prepare("SELECT * FROM tbl_end_category WHERE mcat_id=?");
+															//tatement2->execute(array($row1['mcat_id']));
+															//$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+															//foreach ($result2 as $row2) {
+														?>
+
+														<li><a href="product-category.php?id=<?//php echo $row2['ecat_id']; ?>&type=end-category"><?//php echo $row2['ecat_name']; ?></a></li>
+														<?php
+															//}
+														?>
+                                                    </ul>
+                                                </li>
+                                                <?php
+												//}
+												?>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <?php
+									//}
+									?> -->
+
+
+
+
+
+
+
+
 									<li class="has-dropdown">
 										<a href="product-category.php?id=<?php echo $row['tcat_id']; ?>&type=top-category"><?php echo $row['tcat_name']; ?></a>
                                         <ul class="sub-menu">
@@ -476,17 +529,38 @@ foreach ($result as $row) {
             <div class="container">
                 <div class="row">
                     <div class="col-12 d-flex justify-content-between align-items-center">
-                        <div class="mobile-header--left">
-                            <a href="" class="mobile-logo-link">
-                                <svg class="d-block" width="42" height="42" viewBox="0 0 612 612" xmlns="http://www.w3.org/2000/svg" focusable="false"><title>Bootstrap</title><path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"></path><path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"></path></svg>
+                        <div class="col-4 mobile-header--left">
+                            <a href="cart.php"><img src="https://img.icons8.com/ios/15/000000/shopping-bag.png"/><!-- <?//php echo LANG_VALUE_19; ?> --> (<?php echo LANG_VALUE_1; ?><?php
+					if(isset($_SESSION['cart_p_id'])) {
+						$table_total_price = 0;
+						$i=0;
+	                    foreach($_SESSION['cart_p_qty'] as $key => $value) 
+	                    {
+	                        $i++;
+	                        $arr_cart_p_qty[$i] = $value;
+	                    }                    $i=0;
+	                    foreach($_SESSION['cart_p_current_price'] as $key => $value) 
+	                    {
+	                        $i++;
+	                        $arr_cart_p_current_price[$i] = $value;
+	                    }
+	                    for($i=1;$i<=count($arr_cart_p_qty);$i++) {
+	                    	$row_total_price = $arr_cart_p_current_price[$i]*$arr_cart_p_qty[$i];
+	                        $table_total_price = $table_total_price + $row_total_price;
+	                    }
+						echo $table_total_price; //view cart
+					} else {
+						echo '0.00';
+					}
+					?>)</a>
+                        </div>
+                        <div class="col-4 mobile-header--left">
+                            <a href="index.php" class="mobile-logo-link">
+                                <img src="assets/uploads/<?php echo $logo; ?>" height="70%" width="70%" alt="logo image">
                             </a>
                         </div>
-                        <div class="mobile-header--left" >
-                            <a href="" class="mobile-logo-link">
-                                <svg class="d-block" width="42" height="42" viewBox="0 0 612 612" xmlns="http://www.w3.org/2000/svg" focusable="false"><title>Bootstrap</title><path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"></path><path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"></path></svg>
-                            </a>
-                        </div>
-                        <div class="mobile-header--right">
+
+                        <div class="col-4 mobile-header--right">
                             <a href="#mobile-menu-offcanvas" class="mobile-menu offcanvas-toggle">
                                 <span class="mobile-menu-dash"></span>
                                 <span class="mobile-menu-dash"></span>
@@ -510,6 +584,13 @@ foreach ($result as $row) {
             <!-- Start Mobile Menu User Top -->
             <div class="mobile-menu-top">
                 <span>Welcome to our store!</span>
+                <form class="navbar-form navbar-left" role="search" action="search-result.php" method="get">
+					<?php $csrf->echoInputField(); ?>
+					<div class="form-group">
+						<input type="text" class="form-control search-top" placeholder="<?php echo LANG_VALUE_2; ?>" name="search_text">
+					</div>
+					<button type="submit" class="btn btn-default"><i class="fa fa-search"></i><?//php echo LANG_VALUE_3; ?></button>
+				</form>
             </div> <!-- End Mobile Menu User Top -->
             <!-- Start Mobile Menu Center -->
             <div class="mobile-menu-center">
