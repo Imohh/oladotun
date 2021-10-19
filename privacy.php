b7faf898d1ec47cc2114e69b159c9d7c3556564b
Imoh Precious <jaypee88830@gmail.com>
2021-10-19 02:17:46 +0100

    new commit

---
+++ privacy.php
@@ -0,0 +1,23 @@
+<?php require_once('header.php'); ?>
+
+
+
+
+INFORMATION ABOUT YOU 
+
+Our Privacy Policy governs the transmission of personal information through Eminence By GTX. 
+
+OMISSIONS, INACCURACIES, AND ERRORS 
+
+On rare occasions, information on our site or in the Service may contain typographical errors, inaccuracies, or omissions relating to product descriptions, pricing, promotions, offers, product shipping charges, transit times, or availability. If any information in the Service or on any connected website is inaccurate, we have the right to remedy any mistakes, inaccuracies, or omissions, and to change or update information or cancel orders at any time without prior notice (including after you have submitted your order).
+
+ Except as required by law, we make no commitment to update, revise, or clarify anything in the Service or on any related website, including, without limitation, price information. No specified update or refresh date in the Service or on any linked website should be interpreted to mean that all information in the Service or on any associated website has been modified or updated. 
+
+
+
+
+
+
+
+
+<?php require_once('footer.php'); ?>
