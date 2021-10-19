b6ca1b77ca36461bed7efb9f8a0c28749f0f5b23
Imoh Precious <jaypee88830@gmail.com>
2021-10-19 02:30:41 +0100

    new commit

--- privacy.php
+++ privacy.php
@@ -6,15 +6,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				INFORMATION ABOUT YOU 
+				<div class="policy-paragraph">
+					<strong>INFORMATION ABOUT YOU </strong>
 
-				Our Privacy Policy governs the transmission of personal information through Eminence By GTX. 
+					<p>Our Privacy Policy governs the transmission of personal information through Eminence By GTX.</p>
 
-				OMISSIONS, INACCURACIES, AND ERRORS 
+					<strong>OMISSIONS, INACCURACIES, AND ERRORS</strong>
 
-				On rare occasions, information on our site or in the Service may contain typographical errors, inaccuracies, or omissions relating to product descriptions, pricing, promotions, offers, product shipping charges, transit times, or availability. If any information in the Service or on any connected website is inaccurate, we have the right to remedy any mistakes, inaccuracies, or omissions, and to change or update information or cancel orders at any time without prior notice (including after you have submitted your order).
+					<p>On rare occasions, information on our site or in the Service may contain typographical errors, inaccuracies, or omissions relating to product descriptions, pricing, promotions, offers, product shipping charges, transit times, or availability. If any information in the Service or on any connected website is inaccurate, we have the right to remedy any mistakes, inaccuracies, or omissions, and to change or update information or cancel orders at any time without prior notice (including after you have submitted your order).</p>
 
-				 Except as required by law, we make no commitment to update, revise, or clarify anything in the Service or on any related website, including, without limitation, price information. No specified update or refresh date in the Service or on any linked website should be interpreted to mean that all information in the Service or on any associated website has been modified or updated. 
+					<p>Except as required by law, we make no commitment to update, revise, or clarify anything in the Service or on any related website, including, without limitation, price information. No specified update or refresh date in the Service or on any linked website should be interpreted to mean that all information in the Service or on any associated website has been modified or updated.</p>
+				</div>
 			</div>
 		</div>
 	</div>
