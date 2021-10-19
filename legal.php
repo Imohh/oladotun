ed7755082a8129bcf84e522253bcc5b67ef5f866
Imoh Precious <jaypee88830@gmail.com>
2021-10-19 02:57:47 +0100

    new commit

---
+++ legal.php
@@ -0,0 +1,190 @@
+<?php require_once('header.php'); ?>
+
+
+
+<section class="top-policy">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2>Terms and Conditions</h2>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="policy">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="policy-paragraph">
+					
+					TERMS AND CONDITIONS
+
+					<p>This website is operated by Eminence By GTX. Throughout the site, the terms “we”, “us” and “our” refer to Eminence By GTX. Eminence By GTX provides this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
+					By visiting our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply to all users of the site, including without limitation users who are browsers, customers, and/ or contributors of content.</p>
+
+					<p>Before accessing or using our website, please read these Terms of Service carefully. You agree to be bound by these Terms of Service by accessing or using any element of the site. You may not visit the website or use any services if you do not agree to all of the terms and conditions of this agreement. Acceptance is solely restricted to these Terms of Service if these Terms of Service are considered an offer.</p>
+
+					<p>The Terms of Service will apply to any new features or tools that are added to the current shop. On this page, you can examine the most recent version of the Terms of Service at any time. By making updates and/or changes to our website, we retain the right to update, amend, or replace any portion of these Terms of Service. It is your duty to check this page for updates on a regular basis. Following the publishing of such modifications, your continued use of or access to the website implies acceptance of those changes.</p>
+
+					<p>You signify that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and have given us your agreement to allow any of your minor dependents to use this site, by agreeing to these Terms of Service. </p>
+
+					<p>You may not use our products for any illegal or unauthorized purpose, and you may not break any laws in your jurisdiction by using the Service (including but not limited to copyright laws). </p>
+
+					<p>You must not send any worms, viruses, or any other disruptive programming. </p>
+
+					<p>Any breach or violation of the Terms will result in your Services being immediately terminated.</p>
+
+
+
+
+
+
+
+
+
+
+					<strong>CONDITIONS</strong>
+
+					<p>We have the right, at any moment, to refuse service to anybody for any reason. 
+					You understand that your content (excluding credit card information) may be sent unencrypted over various networks and may require (a) changes to conform and adapt to the technical requirements of connecting networks or devices; and (b) changes to conform and adapt to the technical requirements of connecting networks or devices. </p>
+					
+					<p>Without our express written consent, you undertake not to replicate, duplicate, copy, sell, resell or exploit any component of the Service, use of the Service, or access to the Service or any contact on the website through which the service is given. </p>
+
+					<p>We reserve the right to make changes to the site's content at any time, but we are under no obligation to do so.</p>
+
+					<strong>PRICING</strong>
+
+					<p>Our product prices are subject to change at any time. </p>
+
+					<p>We retain the right to change or cancel the Service (or any element or content of it) at any time and without notice. 
+					Any modification, pricing change, suspension, or discontinuance of the Service will not be liable to you or any third party.</p>
+
+
+
+					<strong>ERVICES OR PRODUCTS (if applicable) </strong>
+
+					<p>Not all items will be available for purchase online. Depending on the trends and the need for change, certain products or services may be available/or may not be available.</p>
+					
+					<p>We have made every attempt to display the colors and images of our products that show on the web store as precisely as possible. We cannot guarantee that the color seen on your computer monitor is correct.</p>
+					
+					<p>We reserve the right, but not the obligation, to limit the sale of our products and services to any individual, geographic location, or jurisdiction. On a case-by-case basis, we may exercise this power. We have the right to limit the number of products or services we offer at any time. All descriptions of products or product pricing are subject to change at any time without notice, at the sole discretion of us. We reserve the right to discontinue any product at any time. Any offer for any product or service made on this site is void where prohibited.</p>
+
+
+					<strong>BILLING AND ACCOUNT INFORMATION ACCURACY</strong>
+
+					<p>Any order you place with us is subject to our discretion. We reserve the right to limit or cancel quantities ordered per person, per household, or per order at our sole discretion. Orders placed by or under the same customer account, the same payment card, and/or orders that use the same billing and/or shipping address may be subject to these restrictions. We may seek to tell you if we make a change to or cancel an order by contacting the e-mail address and/or billing address/phone number supplied at the time the purchase was placed. </p>
+
+					<p>For any purchases made at our store, you agree to provide current, complete, and accurate purchase and account information. You pledge to respond as soon as possible.</p>
+
+
+					<strong>LINKS TO THIRD-PARTY WEBSITES </strong>
+
+					<p>Third-party materials may be included in some of the content, goods, and services offered through our Service. 
+					This site's third-party links may take you to websites that are not linked with us. We are not responsible for examining or assessing the content or accuracy of any third-party materials or websites, or any other materials, products, or services of third parties, and we do not warrant and will not have any duty or obligation for them.</p>
+
+					<p>We are not responsible for any losses or damages incurred as a result of the purchase or usage of goods, services, resources, content, or any other transactions conducted through third-party websites. Please read the following information carefully.</p>
+
+
+
+					<strong>COMMENTS, FEEDBACK, AND OTHER SUBMISSIONS FROM THE USERS</strong>
+
+					<p>We are and shall be under no duty (1) to keep any comments or information confidential; (2) to provide compensation for any comments or information; or (3) to respond to any controversial comments if you send them to us at our request. 
+					We reserve the right to monitor, edit, or delete content that we deem to be unlawful, offensive, threatening, libellous, libelous, pornographic, obscene, or otherwise objectionable, or that infringes any party's intellectual property or these Terms of Service in our sole discretion. </p>
+					
+					<p>You agree that your comments will not infringe on any third-party rights, such as copyright, trademark, privacy, or reputation.</p>
+					<p>You may not impersonate someone else, use a false e-mail address, or otherwise mislead us or third parties as to the origin of any remarks. Any comments you make, as well as their correctness, are solely your responsibility. We accept no responsibility or liability for any remarks made by you or any third party.</p>
+
+
+
+					<strong>INFORMATION ABOUT YOU</strong>
+
+					<p>Our Privacy Policy governs the transmission of personal information through Eminence By GTX.</p>
+
+					<strong>OMISSIONS, INACCURACIES, AND ERRORS </strong>
+
+					<p>On rare occasions, information on our site or in the Service may contain typographical errors, inaccuracies, or omissions relating to product descriptions, pricing, promotions, offers, product shipping charges, transit times, or availability. If any information in the Service or on any connected website is inaccurate, we have the right to remedy any mistakes, inaccuracies, or omissions, and to change or update information or cancel orders at any time without prior notice (including after you have submitted your order).</p>
+
+					<p>Except as required by law, we make no commitment to update, revise, or clarify anything in the Service or on any related website, including, without limitation, price information. No specified update or refresh date in the Service or on any linked website should be interpreted to mean that all information in the Service or on any associated website has been modified or updated. </p>
+
+
+					<strong>USES THAT ARE PROHIBITED </strong>
+
+					<p>You are prohibited from using the site or its content, in addition to other restrictions specified in the Terms of Service: </p>
+					<ul>
+						<li>(a) with any nefarious intent; </li>
+						<li>(b) to violate any international, federal, or state regulation;</li>
+						<li>(c) to solicit anyone to commit or assist in any unlawful activities;</li>
+						<li>(d) to harass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate on the basis of gender, sexual orientation, religion, ethnicity, race, age, national origin, or disability;</li>
+						<li>(e) to submit false or misleading information;</li>
+						<li>(f) to upload or transmit viruses or any other type of malicious code that will or may affect the functionality or operation of the website;</li>
+						<li>(g) to collect or track the personal information of others;</li>
+						<li>(h) spam, phish, pharm, pretext, spider, crawl, or scrape;</li>
+						<li>(i) for any obscene or immoral purpose; or (j) to tamper with or circumvent the security elements of the Service or any linked website, other websites, or the Internet.</li>
+					</ul>
+					<p>For breaching any of the prohibited uses, we retain the right to suspend your usage of the Service or any connected website.</p>
+
+
+					<strong>LIMITATION OF LIABILITY; DISCLAIMER OF WARRANTIES</strong>
+
+					<p>We do not promise, represent, or assure that your use of our service will be error-free, speedy, secure, or uninterrupted. </p>
+					<p>You accept that we may suspend the service for an extended amount of time or cancel it at any moment, with or without notice to you. </p>
+
+					<strong>INDEMNIFICATION </strong>
+
+					<p>You agree to defend, indemnify, and hold Eminence harmless. By GTX suppliers, interns, and employees, you agree to indemnify and hold GTX harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or a third-rights. </p>
+
+
+
+					<strong>TERMINATION </strong>
+
+					<p>For all reasons, the parties' responsibilities and liabilities accrued prior to the termination date will survive the termination of this agreement. </p>
+					<p>Unless and until either you or us terminate these Terms of Service, they will remain in effect. You may terminate these Terms of Service at any time by notifying us that you no longer want to utilize our Services or by discontinuing your use of our website. </p>
+					<p>We may also terminate this agreement at any time without notice if you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service, and you will remain liable for all amounts due up to and including the date of termination; and/or if you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service; and/or if you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service</p>
+
+
+					Support @Eminencebygtx.com
+
+
+
+
+
+
+
+
+
+
+					<strong>RETURNS POLICY</strong>
+
+					<p>Unless unfinished, Eminence By GTX wears are non-returnable and non-refundable. While we will advise and help you with fabric and design options, the final decision is yours to make, and we cannot be held liable. </p>
+
+					<p>Returns or exchanges are not allowed for any reason, including disapproval of style, fit, or colour, event cancellation, or any other reason. When it comes to consumers’ responsibility, it is the customers' responsibility.</p>
+
+					<p>Our policy is valid for  ( )days. We will not offer you a refund or exchange after your purchase. </p>
+
+					<p>Your item must be unused and in the same condition as when you received it to be eligible for a return. It has to be in its original box as well.</p>
+
+
+					<p>Once a fabric is chosen, paid for, and purchased, it is non-refundable, and all other options.</p>
+
+					<p>At the start of the project, a customer must pay a (  ) deposit for our labour, and by doing so, they agree to our terms and conditions. When you order, you will be asked to pay in full at the time you place your order.</p>
+
+					<p>Minor alterations on the clothes apart first fit in the real fabric will not be included in our quotations; any additional alterations will be paid at your discretion. </p>
+
+					<p>We will not be held liable for delays beyond our control if times and dates indicated for the completion of products are not achievable. We will not be held liable financially too.</p>
+
+					<p>When adjustments to customers' garments are discussed, written down, and the customer leaves their item(s) with us, it is the customer's permission for us to undertake the work. Payment is payable upon completion of the job if it is completed to a satisfactory standard and as agreed. Any additional work would be billed to the client.</p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+<?php require_once('footer.php'); ?>
