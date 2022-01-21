d78b31ce6edfc876537e3b02d50554f523a398c9
Imoh Precious <jaypee88830@gmail.com>
2022-01-21 16:29:02 +0100

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -370,6 +370,36 @@ if(!isset($_SESSION['cart_p_id'])) {
 		                            
 		                        
 		                    </div>
+
+                            <h1>Hey there</h1>
+
+
+                            <!-- PAYSTACK INTEGRATION -->
+                            <form id="paymentForm">
+                              <div class="form-group">
+                                <label for="email">Email Address</label>
+                                <input type="email" id="email-address" required />
+                              </div>
+                              <div class="form-group">
+                                <label for="amount">Amount</label>
+                                <input type="tel" id="amount" required />
+                              </div>
+                              <div class="form-group">
+                                <label for="first-name">First Name</label>
+                                <input type="text" id="first-name" />
+                              </div>
+                              <div class="form-group">
+                                <label for="last-name">Last Name</label>
+                                <input type="text" id="last-name" />
+                              </div>
+                              <div class="form-submit">
+                                <button type="submit" onclick="payWithPaystack().the"> Pay </button>
+                              </div>
+                            </form>
+
+
+
+
 		                <?php endif; ?>
                         
                 </div>
@@ -381,6 +411,35 @@ if(!isset($_SESSION['cart_p_id'])) {
         </div>
     </div>
 </div>
+
+<!-- PAYSTACK SCRIPT -->
+<script>
+    const paymentForm = document.getElementById('paymentForm');
+    paymentForm.addEventListener("submit", payWithPaystack, false);
+    function payWithPaystack(e) {
+      e.preventDefault();
+      let handler = PaystackPop.setup({
+        key: 'pk_test_7114661f512e87772aefeab61d6811334b084c85', // Replace with your public key
+        email: document.getElementById("email-address").value,
+        amount: document.getElementById("amount").value * 100,
+        ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+        // label: "Optional string that replaces customer email"
+        onClose: function(){
+          alert('Window closed.');
+        },
+        callback: function(response){
+          let message = 'Payment complete! Reference: ' + response.reference;
+          alert(message);
+          //write function for cart items
+        }
+      });
+      handler.openIframe();
+    }
+</script>
+<script src="https://js.paystack.co/v1/inline.js"></script> 
+
+
+
 
 
 <?php require_once('footer.php'); ?>
